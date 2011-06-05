12fbfab9cffabe5ac66fee02a234aff0d0deae7d
Michael Mifsud <xzyfer@gmail.com>
2011-06-05 12:02:34 +1000

    fix quake/share not displaying

--- symfony/apps/frontend/modules/quake/templates/shareSuccess.php
+++ symfony/apps/frontend/modules/quake/templates/shareSuccess.php
@@ -1,1 +1,10 @@
-<p>Push your story to your Facebook wall.</p>
+<div id="home">
+
+  <div data-role="page" id="foo">
+    <div id="homeHeader">
+      <h1>QuakeFelt</h1>
+    </div>
+    <p>Push your story to your Facebook wall.</p>
+  </div>
+
+</div>
